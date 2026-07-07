12e8748373658683e093ca5c91501697dcf458d0
get-some-code <monideepmistry365@gmail.com>
2026-07-07 16:53:53 +0530

    Updated DELETE endpoint

--- 3. HTTP Methods/4. DELETE/main.py
+++ 3. HTTP Methods/4. DELETE/main.py
@@ -2,26 +2,11 @@
 from fastapi import FastAPI, Path, Query, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Optional
 from pathlib import Path as FilePath
 import json
 
-# ---------------------------------------------------------------------
-# Path to patients.json
-# Project Structure:
-#
-# FASTAPI/
-# │── patients.json
-# └── 3. HTTP Methods/
-#     └── 2. POST/
-#         └── main.py
-# ---------------------------------------------------------------------
-
 DATA_FILE = FilePath(__file__).resolve().parent.parent / "patients.json"
-
-# ---------------------------------------------------------------------
-# Pydantic Model
-# ---------------------------------------------------------------------
 
 class Patient(BaseModel):
     id: Annotated[
@@ -97,18 +82,58 @@ class Patient(BaseModel):
             return "Overweight"
 
         return "Obese"
+    
+class PatientUpdate(BaseModel):
 
+    name: Annotated[
+        Optional[str],
+        Field(
+            default=None,
+            max_length=50,
+        )
+    ]
 
-# ---------------------------------------------------------------------
-# FastAPI App
-# ---------------------------------------------------------------------
+    city: Annotated[
+        Optional[str],
+        Field(
+            default=None,
+            max_length=20,
+        )
+    ]
+
+    age: Annotated[
+        Optional[int],
+        Field(
+            default=None,
+            gt=0,
+            lt=120,
+        )
+    ]
+
+    gender: Annotated[
+        Optional[Literal["male", "female", "others"]],
+        Field(default=None, description="Gender of the patient")
+    ]
+
+    height: Annotated[
+        Optional[float],
+        Field(
+            default=None,
+            gt=0,
+            description="Height in meters"
+        )
+    ]
+
+    weight: Annotated[
+        Optional[float],
+        Field(
+            default=None,
+            gt=0,
+            description="Weight in kilograms"
+        )
+    ]
 
 app = FastAPI()
-
-
-# ---------------------------------------------------------------------
-# Helper Functions
-# ---------------------------------------------------------------------
 
 def load_data():
 
@@ -121,11 +146,6 @@ def save_data(data):
     with open(DATA_FILE, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
 
-
-# ---------------------------------------------------------------------
-# Home
-# ---------------------------------------------------------------------
-
 @app.get("/")
 def homePage():
     return {
@@ -133,29 +153,15 @@ def homePage():
     }
 
 
-# ---------------------------------------------------------------------
-# About
-# ---------------------------------------------------------------------
-
 @app.get("/about")
 def aboutPage():
     return {
         "message": "A fully functional API to manage your patient records."
     }
 
-
-# ---------------------------------------------------------------------
-# Get all patients
-# ---------------------------------------------------------------------
-
 @app.get("/patients")
 def allPatientsPage():
     return load_data()
-
-
-# ---------------------------------------------------------------------
-# Get a patient by ID
-# ---------------------------------------------------------------------
 
 @app.get("/patients/{patient_id}")
 def viewPatient(
@@ -175,11 +181,6 @@ def viewPatient(
         )
 
     return data[patient_id]
-
-
-# ---------------------------------------------------------------------
-# Sort patients
-# ---------------------------------------------------------------------
 
 @app.get("/sort")
 def sortPatients(
@@ -220,11 +221,6 @@ def sortPatients(
 
     return sorted_data
 
-
-# ---------------------------------------------------------------------
-# Create a new patient
-# ---------------------------------------------------------------------
-
 @app.post("/create", status_code=201)
 def createPatient(patient: Patient):
 
@@ -244,6 +240,32 @@ def createPatient(patient: Patient):
     return {
         "message": "Patient created successfully."
     }
+
+@app.put("/edit/{patient_id}")
+def update_patient(patient_id: str, patient_update: PatientUpdate):
+
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail="Oops! Patient id not found")
+    
+    existing_patient_info = data[patient_id]
+
+    updated_patient_info = patient_update.model_dump(exclude_unset=True)
+
+    for key, value in updated_patient_info.items():
+        existing_patient_info[key] = value
+
+    existing_patient_info['id'] = patient_id
+    patient_pydantic_obj = Patient(**existing_patient_info)
+    existing_patient_info = patient_pydantic_obj.model_dump(exclude={'id'})
+
+    data[patient_id] = existing_patient_info
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message': "Patient updated successfully"})
+
 
 @app.delete("/delete/{patient_id}")
 def deletePatient(patient_id):
